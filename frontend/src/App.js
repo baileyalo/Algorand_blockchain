c52c9a7b0505061542e8247abdf611dc9ac691b2
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-03-25 20:25:58 -0400

    update react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './algorand-algo-logo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+       
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.algorand.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Algorand
         </a>
       </header>
     </div>
